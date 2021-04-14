0e8e85097ffc59c6ed7eea634d2800dd3abbe5d3
kh737v <kaihus@yahoo.com>
2021-04-14 19:04:48 -0400

    Add print os function and another factorial func

--- basic_python.py
+++ basic_python.py
@@ -1,7 +1,27 @@
 
 # Factorial with lambda and math.factorial
+import os 
+
+if __name__ == "__main__": 
+    for (root,dirs,files) in os.walk('.', topdown=1): 
+        print (root) 
+        print (dirs) 
+        print (files) 
+        print ('--------------------------------')
+
 import math
 
 y = lambda a : math.factorial(a)
-a = 15 #input("Enter a number: ")
-print(a, '! = ', y(a))
+a = 5 #input("Enter a number: ")
+prinit(a, '! = ', y(a))
+
+# Factorial
+def factorial(x):
+    if x == 1:
+        return x
+    else:
+        return x*factorial(x-1)
+    
+n = 6 #input("Enter a number: ")
+print( n, '!= ', factorial( int(n) ) )
+
